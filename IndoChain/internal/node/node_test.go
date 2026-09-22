037ce44988388f2d4cae077a4fe19e2f0a82c1b3
Antoni Oktha Fernandes <37358597+DesKaOne@users.noreply.github.com>
2026-09-22 22:14:49 +0700

    test: cover failed block commit atomicity

--- IndoChain/internal/node/node_test.go
+++ IndoChain/internal/node/node_test.go
@@ -1,6 +1,7 @@
 package node
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/DesKaOne/DesKaEcosystem/IndoChain/genesis/devnet"
@@ -11,6 +12,20 @@ import (
 	"github.com/DesKaOne/DesKaEcosystem/IndoChain/internal/crypto"
 	"github.com/DesKaOne/DesKaEcosystem/IndoChain/internal/storage"
 )
+
+var errCommitFailed = errors.New("commit failed")
+
+type failingCommitStore struct {
+	*storage.MemoryStore
+	failCommit bool
+}
+
+func (s *failingCommitStore) CommitBlockState(b block.Block, hash types.Hash, st *state.State) error {
+	if s.failCommit {
+		return errCommitFailed
+	}
+	return s.MemoryStore.CommitBlockState(b, hash, st)
+}
 
 func TestNewDevnetInitializesGenesis(t *testing.T) {
 	store := storage.NewMemoryStore()
@@ -349,5 +364,102 @@ func TestImportBlockRejectsWrongParentWithoutMutation(t *testing.T) {
 	}
 	if n.Head.Header.Height != 0 || n.State.Root() != before {
 		t.Fatal("node mutated after rejecting invalid parent")
+	}
+}
+
+func TestImportBlockCommitFailureWithoutMutation(t *testing.T) {
+	store := &failingCommitStore{MemoryStore: storage.NewMemoryStore()}
+	n, err := NewDevnet(store)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	seed := make([]byte, 32)
+	seed[0] = 11
+	keyPair, err := crypto.NewEd25519KeyPair(seed)
+	if err != nil {
+		t.Fatal(err)
+	}
+	signer, err := crypto.NewEd25519Signer(keyPair.PrivateKey)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	sender := types.Address([]byte("commit-failure-sender"))
+	recipient := types.Address([]byte("commit-failure-recipient"))
+	n.State.Set(sender, state.Account{Balance: 100, Nonce: 0})
+	beforeHead := n.Head
+	beforeHash := n.HeadHash
+	beforeStateRoot := n.State.Root()
+	storedBeforeHead, storedBeforeHash, err := store.Head()
+	if err != nil {
+		t.Fatal(err)
+	}
+	storedBeforeState, err := store.LoadState()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	tx := transaction.Transaction{
+		Version: devnet.ProtocolVersion,
+		ChainID: devnet.ChainID,
+		Nonce: 0,
+		Sender: sender,
+		Recipient: recipient,
+		Value: 30,
+		GasLimit: 100,
+	}
+	sig, err := transaction.Sign(tx, signer)
+	if err != nil {
+		t.Fatal(err)
+	}
+	tx.Signature = sig
+
+	rules, err := n.Config.BlockRules(signer.PublicKey())
+	if err != nil {
+		t.Fatal(err)
+	}
+	working := n.State.Snapshot()
+	if err := state.ApplyTransaction(working, tx, rules.Transaction); err != nil {
+		t.Fatal(err)
+	}
+
+	next := block.Block{
+		Header: block.Header{
+			Version: devnet.ProtocolVersion,
+			ChainID: devnet.ChainID,
+			Height: 1,
+			Timestamp: n.Head.Header.Timestamp + 1,
+			PreviousHash: n.HeadHash,
+			StateRoot: working.Root(),
+		},
+		Transactions: []any{tx},
+	}
+	next.Header.TransactionsRoot, err = block.TransactionsRoot(next.Transactions)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	store.failCommit = true
+	if err := n.ImportBlock(next, signer.PublicKey()); !errors.Is(err, errCommitFailed) {
+		t.Fatalf("error = %v, want %v", err, errCommitFailed)
+	}
+
+	if n.Head != beforeHead || n.HeadHash != beforeHash || n.State.Root() != beforeStateRoot {
+		t.Fatal("node mutated after failed block commit")
+	}
+	storedAfterHead, storedAfterHash, err := store.Head()
+	if err != nil {
+		t.Fatal(err)
+	}
+	storedAfterState, err := store.LoadState()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if storedAfterHead != storedBeforeHead || storedAfterHash != storedBeforeHash {
+		t.Fatal("store head mutated after failed block commit")
+	}
+	if storedAfterState.Root() != storedBeforeState.Root() {
+		t.Fatal("store state mutated after failed block commit")
 	}
 }
