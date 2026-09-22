ec25f79b91ed4f4dbbaa6d1ffbf237ddf4fc73a1
Antoni Oktha Fernandes <37358597+DesKaOne@users.noreply.github.com>
2026-09-22 21:12:29 +0700

    IndoChain: bind node startup to chain config

--- IndoChain/internal/node/node.go
+++ IndoChain/internal/node/node.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/DesKaOne/DesKaEcosystem/IndoChain/genesis/devnet"
+	"github.com/DesKaOne/DesKaEcosystem/IndoChain/internal/config"
 	"github.com/DesKaOne/DesKaEcosystem/IndoChain/internal/core/block"
 	"github.com/DesKaOne/DesKaEcosystem/IndoChain/internal/core/state"
 	"github.com/DesKaOne/DesKaEcosystem/IndoChain/internal/core/types"
@@ -19,6 +20,7 @@ var (
 )
 
 type Node struct {
+	Config   config.ChainConfig
 	Genesis  devnet.Genesis
 	Store    storage.ChainStore
 	State    *state.State
@@ -30,7 +32,14 @@ func NewDevnet(store storage.ChainStore) (*Node, error) {
 	if store == nil {
 		return nil, ErrNilStore
 	}
+	chainConfig := config.Devnet()
+	if err := chainConfig.Validate(); err != nil {
+		return nil, err
+	}
 	genesis := devnet.Default()
+	if chainConfig.NetworkProfile != genesis.NetworkProfile || chainConfig.ChainID != genesis.ChainID || chainConfig.ProtocolVersion != genesis.ProtocolVersion {
+		return nil, ErrGenesisMismatch
+	}
 	genesisBlock, err := genesis.Block()
 	if err != nil {
 		return nil, err
@@ -47,7 +56,7 @@ func NewDevnet(store storage.ChainStore) (*Node, error) {
 		return nil, err
 	}
 	return &Node{
-		Genesis: genesis, Store: store, State: initialState.Snapshot(),
+		Config: chainConfig, Genesis: genesis, Store: store, State: initialState.Snapshot(),
 		Head: genesisBlock, HeadHash: genesisHash,
 	}, nil
 }
