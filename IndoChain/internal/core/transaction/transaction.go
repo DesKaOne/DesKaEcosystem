fbb8cb79cd093cb030e0ffb3a3da060d93d32ce8
Antoni Oktha Fernandes <37358597+DesKaOne@users.noreply.github.com>
2026-09-22 19:57:45 +0700

    IndoChain: use protocol Address abstraction in transactions

--- IndoChain/internal/core/transaction/transaction.go
+++ IndoChain/internal/core/transaction/transaction.go
@@ -2,12 +2,15 @@ package transaction
 
 import "github.com/DesKaOne/DesKaEcosystem/IndoChain/internal/core/types"
 
+// Transaction is the development transaction model.
+// Sender and Recipient use the protocol Address abstraction so the final
+// wire format can remain independent from a specific address encoding.
 type Transaction struct {
 	Version   types.ProtocolVersion
 	ChainID   types.ChainID
 	Nonce     types.Nonce
-	Sender    []byte
-	Recipient []byte
+	Sender    types.Address
+	Recipient types.Address
 	Value     uint64
 	GasLimit  uint64
 	Data      []byte
