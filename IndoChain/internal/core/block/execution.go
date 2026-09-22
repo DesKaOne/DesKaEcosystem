fe06890c3cbbcaefd0d6dc487097a6355439e02a
Antoni Oktha Fernandes <37358597+DesKaOne@users.noreply.github.com>
2026-09-22 20:37:21 +0700

    feat(IndoChain): add atomic block execution layer

---
+++ IndoChain/internal/core/block/execution.go
@@ -0,0 +1,64 @@
+package block
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/DesKaOne/DesKaEcosystem/IndoChain/internal/core/state"
+	"github.com/DesKaOne/DesKaEcosystem/IndoChain/internal/core/transaction"
+	"github.com/DesKaOne/DesKaEcosystem/IndoChain/internal/core/types"
+)
+
+var (
+	ErrNilState        = errors.New("nil execution state")
+	ErrWrongChainID    = errors.New("invalid block chain id")
+	ErrWrongVersion    = errors.New("invalid block protocol version")
+	ErrHeightMismatch  = errors.New("invalid block height")
+	ErrPreviousHash    = errors.New("invalid previous hash")
+	ErrTransactionType = errors.New("invalid block transaction type")
+)
+
+type ExecutionRules struct {
+	ChainID         types.ChainID
+	ProtocolVersion types.ProtocolVersion
+	Transaction     state.ExecutionRules
+}
+
+func ValidateHeader(b Block, expectedHeight types.Height, expectedPreviousHash types.Hash, rules ExecutionRules) error {
+	if b.Header.ChainID != rules.ChainID {
+		return ErrWrongChainID
+	}
+	if b.Header.Version != rules.ProtocolVersion {
+		return ErrWrongVersion
+	}
+	if b.Header.Height != expectedHeight {
+		return ErrHeightMismatch
+	}
+	if b.Header.PreviousHash != expectedPreviousHash {
+		return ErrPreviousHash
+	}
+	return nil
+}
+
+func ExecuteBlock(s *state.State, b Block, expectedHeight types.Height, expectedPreviousHash types.Hash, rules ExecutionRules) error {
+	if s == nil {
+		return ErrNilState
+	}
+	if err := ValidateHeader(b, expectedHeight, expectedPreviousHash, rules); err != nil {
+		return err
+	}
+
+	working := s.Snapshot()
+	for index, rawTx := range b.Transactions {
+		tx, ok := rawTx.(transaction.Transaction)
+		if !ok {
+			return fmt.Errorf("%w at index %d", ErrTransactionType, index)
+		}
+		if err := state.ApplyTransaction(working, tx, rules.Transaction); err != nil {
+			return fmt.Errorf("transaction %d: %w", index, err)
+		}
+	}
+
+	s.Replace(working)
+	return nil
+}
