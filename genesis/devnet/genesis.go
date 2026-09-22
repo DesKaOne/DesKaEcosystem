db6182bd402f32d943ad8c2ae31911a5a6cdb93c
Antoni Oktha Fernandes <37358597+DesKaOne@users.noreply.github.com>
2026-09-22 19:30:33 +0700

    feat: add devnet genesis model

---
+++ genesis/devnet/genesis.go
@@ -0,0 +1,13 @@
+package devnet
+
+import "github.com/DesKaOne/DesKaEcosystem/internal/core/types"
+
+type Genesis struct {
+	ChainID types.ChainID
+	NetworkProfile string
+	ProtocolVersion types.ProtocolVersion
+	Timestamp int64
+	InitialValidators [][]byte
+	InitialAccounts [][]byte
+	InitialAllocations map[string]uint64
+}
