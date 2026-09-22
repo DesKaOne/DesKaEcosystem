bf29c25b7b9512ddead7267009af1a32e6c0d29c
Antoni Oktha Fernandes <37358597+DesKaOne@users.noreply.github.com>
2026-09-22 19:56:39 +0700

    IndoChain: fix development transaction hash ChainID encoding

--- IndoChain/internal/core/transaction/hash.go
+++ IndoChain/internal/core/transaction/hash.go
@@ -11,7 +11,7 @@ func Hash(tx Transaction) types.Hash {
 	// Replace with the frozen canonical transaction encoding before protocol freeze.
 	h := sha256.New()
 	h.Write([]byte{byte(tx.Version >> 8), byte(tx.Version)})
-	h.Write([]byte(tx.ChainID.String()))
+	h.Write([]byte{byte(tx.ChainID >> 56), byte(tx.ChainID >> 48), byte(tx.ChainID >> 40), byte(tx.ChainID >> 32), byte(tx.ChainID >> 24), byte(tx.ChainID >> 16), byte(tx.ChainID >> 8), byte(tx.ChainID)})
 	h.Write([]byte(tx.Sender))
 	h.Write([]byte(tx.Recipient))
 	return sha256.Sum256(h.Sum(nil))
