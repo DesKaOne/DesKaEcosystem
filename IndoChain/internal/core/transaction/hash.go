355a3e36ec737c0662fee70e4a6c949abecb2e04
Antoni Oktha Fernandes <37358597+DesKaOne@users.noreply.github.com>
2026-09-22 20:15:39 +0700

    style: gofmt transaction package

--- IndoChain/internal/core/transaction/hash.go
+++ IndoChain/internal/core/transaction/hash.go
@@ -1,18 +1,18 @@
 package transaction
 
 import (
-    "crypto/sha256"
+	"crypto/sha256"
 
-    "github.com/DesKaOne/DesKaEcosystem/IndoChain/internal/core/types"
+	"github.com/DesKaOne/DesKaEcosystem/IndoChain/internal/core/types"
 )
 
 // Hash returns a development transaction identifier derived from the complete
 // signed transaction encoding. The canonical hash algorithm remains subject
 // to protocol freeze.
 func Hash(tx Transaction) types.Hash {
-    signed, err := SignedBytes(tx)
-    if err != nil {
-        return types.Hash{}
-    }
-    return sha256.Sum256(signed)
+	signed, err := SignedBytes(tx)
+	if err != nil {
+		return types.Hash{}
+	}
+	return sha256.Sum256(signed)
 }
